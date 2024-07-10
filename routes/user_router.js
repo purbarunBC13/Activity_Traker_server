4de695a432b717099d11391a6231e3e2a52b3530
Supriyo-sen <supriyo.webweeb@gmail.com>
2024-07-10 11:19:28 +0530

    almost done

--- routes/user_router.js
+++ routes/user_router.js
@@ -1,8 +1,10 @@
 const upload = require("../configs/multer");
-const {postProgressData} = require("../controllers/progress_controller");
+const {postProgressData , getWorkIds} = require("../controllers/progress_controller");
 const router = require("express").Router();
 
 router.post("/progress", postProgressData);
+
+router.get("/progress", getWorkIds);
 
 router.post("/upload", upload.single("image"), (req, res) => {
   res.send("Image uploaded successfully");
